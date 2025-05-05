b5d639ee24a3e2afbf0786058a79c368dcb60b84
Mingrou Wei <mingrou.wei@uzh.ch>
2025-05-05 21:45:28 +0200

    Fix: use pre-norm for encoder and decoder

--- scripts/train_prenorm.sh
+++ scripts/train_prenorm.sh
@@ -1,3 +1,4 @@
+# train_prenorm.sh
 #! /bin/bash
 
 scripts=$(dirname "$0")
@@ -12,18 +13,18 @@ num_threads=10
 #device=0
 
 # measure time
-
 SECONDS=0
 
 logs=$base/logs
 
-model_name=deen_transformer_pre
+model_name=deen_transformer_prenorm
 
 mkdir -p $logs
-
 mkdir -p $logs/$model_name
 
-OMP_NUM_THREADS=$num_threads python -m joeynmt train $configs/$model_name.yaml > $logs/$model_name/out 2> $logs/$model_name/err
+OMP_NUM_THREADS=$num_threads \
+  python -m joeynmt train $configs/$model_name.yaml \
+  > $logs/$model_name/out 2> $logs/$model_name/err
 
 echo "time taken:"
 echo "$SECONDS seconds"
